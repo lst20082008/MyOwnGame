07b183b361a295c7bd40a189702038a234140394
lst20082008 <sglst123@gmail.com>
2017-04-17 16:17:31 +0800

    增加了角色创建、移动、旋转、下线

---
+++ Assets/Scripts/NetworkManager.cs
@@ -0,0 +1,318 @@
+﻿using SocketIO;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NetworkManager : MonoBehaviour {
+
+    public static NetworkManager instance;
+    public Canvas canvas;
+    public SocketIOComponent socket;
+    public InputField playerNameInput;
+    public GameObject player;
+    public GameObject spawnPoint;
+    public GameObject mainCamera;
+
+    private void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null)
+            Destroy(gameObject);
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        //subscribe to all the various websocket events
+        /*
+        Just show you how to write
+        socket.On("other player connected", OnOtherPlayerConnected);
+        socket.On("play", OnPlay);
+        socket.On("player move", OnPlayerMove);
+        socket.On("player turn", OnPlayerTurn);
+        */
+        socket.On("register",OnRegister);
+        socket.On("log in",OnLogIn);
+        socket.On("other player connected",OnOtherPlayerConnected);
+        socket.On("play",OnPlay);
+        socket.On("player move", OnPlayerMove);
+        socket.On("player turn", OnPlayerTurn);
+        socket.On("other player disconnected", OnOtherPlayerDisconnect);
+    }
+
+    public void BtmRegister()
+    {
+        StartCoroutine(Register());
+    }
+
+    public void BtmLogIn()
+    {
+        StartCoroutine(ConnectToServer());
+    }
+    #region Commands
+
+    IEnumerator ConnectToServer()
+    {
+        yield return new WaitForSeconds(0.5f);
+        string playerName = playerNameInput.text;
+        LogInSendJSON logInSendJSON = new LogInSendJSON(playerName);
+        string data = JsonUtility.ToJson(logInSendJSON);
+        socket.Emit("log in",new JSONObject(data));
+    }
+    
+    IEnumerator Register()
+    {
+        yield return new WaitForSeconds(0.5f);
+        RegisterSendJSON registerSendJSON = new RegisterSendJSON(playerNameInput.text);
+        string data = JsonUtility.ToJson(registerSendJSON);
+        socket.Emit("register",new JSONObject(data));
+    }
+
+    IEnumerator WaitASecond()
+    {
+        yield return new WaitForSeconds(1f);
+    }
+
+    public void CommandPlayerConnect()
+    {
+        socket.Emit("player connect");
+        StartCoroutine(WaitASecond());
+        PlaySendJSON playSendJSON = new PlaySendJSON(playerNameInput.text,spawnPoint);
+        string data = JsonUtility.ToJson(playSendJSON);
+        socket.Emit("play",new JSONObject(data));
+    }
+
+    public void CommandMove(Vector3 vec3)
+    {
+        string data = JsonUtility.ToJson(new PositionJSON(vec3));
+        socket.Emit("player move", new JSONObject(data));
+    }
+
+    public void CommandTurn(Quaternion quar)
+    {
+        string data = JsonUtility.ToJson(new RotationJSON(quar));
+        socket.Emit("player turn", new JSONObject(data));
+    }
+
+    #endregion
+
+    #region Listening
+
+    void OnRegister(SocketIOEvent socketIOEvent)
+    {
+        RegisterJSON registerJSON = RegisterJSON.CreateFromJSON(socketIOEvent.data.ToString());
+        if (registerJSON.state == "error")
+            playerNameInput.text = "账号已被注册";
+        else
+            playerNameInput.text = "账号成功注册";
+    }
+
+    void OnLogIn(SocketIOEvent socketIOEvent)
+    {
+        LogInJSON logInJSON = LogInJSON.CreateFromJSON(socketIOEvent.data.ToString());
+        if (logInJSON.state == "ok")
+        {
+            canvas.gameObject.SetActive(false);
+            CommandPlayerConnect();
+        }
+        else
+            playerNameInput.text = "无此账号!";
+    }
+
+    void OnOtherPlayerConnected(SocketIOEvent socketIOEvent)
+    {
+        PlayerJSON playerJSON = PlayerJSON.CreateFromJSON(socketIOEvent.data.ToString());
+        Vector3 position = new Vector3(playerJSON.position[0], playerJSON.position[1], playerJSON.position[2]);
+        Quaternion rotation = Quaternion.Euler(playerJSON.rotation[0], playerJSON.rotation[1], playerJSON.rotation[2]);
+        GameObject o = GameObject.Find(playerJSON.name) as GameObject;
+        if (o != null)
+        {
+            return;
+        }
+        GameObject p = Instantiate(player, position, rotation) as GameObject;
+        p.name = playerJSON.name;
+        PlayerController pc = p.GetComponent<PlayerController>();
+        pc.playerName = playerJSON.name;
+        pc.isLocalPlayer = false;
+        Transform t = p.transform.Find("Healthbar Canvas");
+        Transform t1 = t.transform.Find("Player Name");
+        Text playerName = t1.GetComponent<Text>();
+        playerName.text = playerJSON.name;
+        //改变p的名字,并且告诉对象不是localplayer等等，并且初始化血量
+    }
+
+    void OnPlay(SocketIOEvent socketIOEvent)
+    {
+        PlayerJSON playerJSON = PlayerJSON.CreateFromJSON(socketIOEvent.data.ToString());
+        Vector3 position = new Vector3(playerJSON.position[0], playerJSON.position[1], playerJSON.position[2]);
+        Quaternion rotation = Quaternion.Euler(playerJSON.rotation[0], playerJSON.rotation[1], playerJSON.rotation[2]);
+        GameObject o = GameObject.Find(playerJSON.name) as GameObject;
+        if (o != null)
+        {
+            return;
+        }
+        GameObject p = Instantiate(player, position, rotation) as GameObject;
+        p.name = playerJSON.name;
+        PlayerController pc = p.GetComponent<PlayerController>();
+        pc.playerName = playerJSON.name;
+        pc.isLocalPlayer = true;
+        mainCamera.transform.position = pc.cameraPosition.transform.position;
+        mainCamera.transform.rotation = pc.cameraPosition.transform.rotation;
+        mainCamera.transform.SetParent(p.transform);
+        pc.cmr = mainCamera;
+        Transform t = p.transform.Find("Healthbar Canvas");
+        Transform t1 = t.transform.Find("Player Name");
+        Text playerName = t1.GetComponent<Text>();
+        playerName.text = playerJSON.name;
+        //改变p的名字等等，并且初始化血量
+    }
+
+    void OnPlayerMove(SocketIOEvent socketIOEvent)
+    {
+        string data = socketIOEvent.data.ToString();
+        PlayerJSON playerJSON = PlayerJSON.CreateFromJSON(data);
+        Vector3 position = new Vector3(playerJSON.position[0], playerJSON.position[1], playerJSON.position[2]);
+        //if it's the current player exit
+        if (playerJSON.name == playerNameInput.text)
+            return;
+        GameObject p = GameObject.Find(playerJSON.name) as GameObject;
+        if (p != null)
+            p.transform.position = position;
+    }
+
+    void OnPlayerTurn(SocketIOEvent socketIOEvent)
+    {
+        string data = socketIOEvent.data.ToString();
+        PlayerJSON playerJSON = PlayerJSON.CreateFromJSON(data);
+        Quaternion rotation = Quaternion.Euler(playerJSON.rotation[0], playerJSON.rotation[1], playerJSON.rotation[2]);
+        //if it's the current player exit
+        if (playerJSON.name == playerNameInput.text)
+            return;
+        GameObject p = GameObject.Find(playerJSON.name) as GameObject;
+        if (p != null)
+            p.transform.rotation = rotation;
+    }
+
+    void OnOtherPlayerDisconnect(SocketIOEvent socketIOEvent)
+    {
+        print("user disconnected");
+        string data = socketIOEvent.data.ToString();
+        PlayerJSON playerJSON = PlayerJSON.CreateFromJSON(data);
+        Destroy(GameObject.Find(playerJSON.name));
+    }
+
+    #endregion
+
+    #region JSONClasses
+
+    [Serializable]
+    public class RegisterSendJSON
+    {
+        public string name;
+
+        public RegisterSendJSON(string _name)
+        {
+            name = _name;
+        }
+    }
+
+    [Serializable]
+    public class RegisterJSON
+    {
+        public string state;
+
+        public static RegisterJSON CreateFromJSON(string data)
+        {
+            return JsonUtility.FromJson<RegisterJSON>(data);
+        }
+    }
+
+    [Serializable]
+    public class LogInSendJSON
+    {
+        public string name;
+
+        public LogInSendJSON(string _name)
+        {
+            name = _name;
+        }
+    }
+
+    [Serializable]
+    public class LogInJSON
+    {
+        public string state;
+        public string name;
+        public List<int> item;
+
+        public static LogInJSON CreateFromJSON(string data)
+        {
+            return JsonUtility.FromJson<LogInJSON>(data);
+        }
+    }
+
+    [Serializable]
+    public class PlayerJSON
+    {
+        public string name;
+        public float[] position;
+        public float[] rotation;
+        public int health;
+
+        public static PlayerJSON CreateFromJSON(string data)
+        {
+            return JsonUtility.FromJson<PlayerJSON>(data);
+        }
+    }
+
+    [Serializable]
+    public class PlaySendJSON
+    {
+        public string name;
+        public float[] spawnPointPosition;
+        public float[] spawnPointRotation;
+
+        public PlaySendJSON(string _name,GameObject _spawnPoint)
+        {
+            name = _name;
+            spawnPointPosition = new float[] {
+                _spawnPoint.transform.position.x,
+                _spawnPoint.transform.position.y,
+                _spawnPoint.transform.position.z
+            };
+            spawnPointRotation = new float[] {
+                _spawnPoint.transform.eulerAngles.x,
+                _spawnPoint.transform.eulerAngles.y,
+                _spawnPoint.transform.eulerAngles.z
+            };
+        }
+    }
+
+    [Serializable]
+    public class PositionJSON
+    {
+        public float[] position;
+
+        public PositionJSON(Vector3 _position)
+        {
+            position = new float[] { _position.x, _position.y, _position.z };
+        }
+    }
+
+    [Serializable]
+    public class RotationJSON
+    {
+        public float[] rotation;
+
+        public RotationJSON(Quaternion _rotation)
+        {
+            rotation = new float[] { _rotation.eulerAngles.x, _rotation.eulerAngles.y, _rotation.eulerAngles.z };
+        }
+    }
+
+
+    #endregion
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+    //方向灵敏度  
+    public float sensitivityX = 10F;
+    public float sensitivityY = 10F;
+    //上下最大角(Y角)  
+    public float minimumY = -60F;
+    public float maximumY = 60F;
+    public GameObject cmr;
+    float rotationY = 0F;
+
+    public bool isLocalPlayer = false;
+    public string playerName;
+    public GameObject cameraPosition;
+
+    private Vector3 currentPosition;
+    private Vector3 oldPosition;
+    private Quaternion oldRotation;
+    private Quaternion currentRotation;
+
+	// Use this for initialization
+	void Start ()
+    {
+        oldPosition = transform.position;
+        currentPosition = oldPosition;
+        oldRotation = transform.rotation;
+        currentRotation = oldRotation;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (!isLocalPlayer)
+            return;
+        var x = Input.GetAxis("Horizontal") * Time.deltaTime * 3.0f;
+        var y = Input.GetAxis("Vertical") * Time.deltaTime * 3.0f;
+
+        //根据鼠移的快慢(增量), 得相机左右旋的角度(理X)  
+        float rotationX = transform.localEulerAngles.y + Input.GetAxis("Mouse X") * sensitivityX;
+        //根据鼠移的快慢(增量), 得相机上下旋的角度(理Y)  
+        rotationY += Input.GetAxis("Mouse Y") * sensitivityY;
+        //角度限制. rotationY小于min,返回min. 大于max,返回max. 否则返回value   
+        rotationY = Mathf.Clamp(rotationY, minimumY, maximumY);
+        //体置一下相机角度  
+        cmr.transform.localEulerAngles = new Vector3(-rotationY, 0, 0);
+        transform.localEulerAngles = new Vector3(0, rotationX, 0);
+
+        transform.Translate(x, 0, y);
+        currentPosition = transform.position;
+        currentRotation = transform.rotation;
+        if (currentPosition != oldPosition)
+        {
+            //TODO Network
+            NetworkManager.instance.GetComponent<NetworkManager>().CommandMove(transform.position);
+            oldPosition = currentPosition;
+        }
+
+        if (currentRotation != oldRotation)
+        {
+            //TODO Network
+            NetworkManager.instance.GetComponent<NetworkManager>().CommandTurn(transform.rotation);
+            oldRotation = currentRotation;
+        }
+    }
+}
