25b4fec375da851a7b186f47f00f2940a81fb65c
HandsomeLST <sglst123@gmail.com>
2017-06-04 01:00:17 +0800

    添加了血量显示，以及惨叫

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,16 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Health : MonoBehaviour {
 
     public const int maxHealth = 100;
     public int currentHealth = maxHealth;
     public RectTransform healthBar;
+    private PlayerController pc;
+    private Image healthState;
+    private AudioSource audioSource;
 
     // Use this for initialization
     void Start () {
-		
+        pc = this.gameObject.GetComponent<PlayerController>();
+        if (pc.isLocalPlayer == true)
+        {
+            audioSource = gameObject.GetComponent<AudioSource>();
+            healthState = GameObject.Find("healthState").GetComponent<Image>();
+            healthState.color = new Color(0,1f,0);
+        }
 	}
 
     public void TakeDamage(GameObject playerFrom, int amount)
@@ -28,6 +38,16 @@ public class Health : MonoBehaviour {
         {
             Destroy(gameObject);
             currentHealth = 0;
+        }
+        if (pc.isLocalPlayer == true)
+        {
+            audioSource.Play();
+            if (currentHealth < 20)
+                healthState.color = new Color(1f, 1f, 1f);
+            else if (currentHealth < 50)
+                healthState.color = new Color(1f, 0, 0);
+            else if (currentHealth < 80)
+                healthState.color = new Color(1f,0.5f,0);
         }
     }
 }
