980e705dbf7ec9c161f3cd35f87931dd420e1fec
moemurk <murk1011@gmail.com>
2017-07-27 23:41:08 +0900

    magic5 Let's Fly added

---
+++ Assets/Scripts/Magics/Magic5Melee/Lightning.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lightning : MonoBehaviour {
+	public float delayTime;
+	public float force;
+	public int damage;
+	public float radius;
+	public GameObject P;
+	public bool LocalPlayer = false;
+
+	private float timer = 0f;
+
+	void Start()
+	{
+		Debug.Log("进入Lightning"+P);
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+		if (timer >= delayTime) {
+			Kaminari ();
+		}
+	}
+
+	void Kaminari()
+	{
+		Collider[] cols = Physics.OverlapSphere(transform.position,radius);
+		if (cols.Length > 0)
+		{
+			for (int i = 0; i < cols.Length; i++) 
+			{
+				Rigidbody r = cols [i].GetComponent<Rigidbody> ();
+				if (r != LocalPlayer) {
+					if (r != null)
+						r.AddExplosionForce (force, transform.position, radius);
+					Health h = cols [i].GetComponent<Health> ();
+					if (h != null)
+						h.TakeDamage (P, damage);	
+				}
+			}
+		}
+		Destroy (this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Magics/Magic5Melee/Magic5.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Magic5 : MonoBehaviour {
+	public GameObject lightning;
+	public float distence;
+	public GameObject P;
+
+	void Start()
+	{
+		Vector3 position = this.transform.position + this.transform.forward*distence;
+		Instantiate(lightning, position, this.transform.rotation).GetComponent<FireBoom>().P = P;
+		Destroy (this.gameObject);
+	}
+
+}
